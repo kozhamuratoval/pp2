94a838e867feb7a3ee27ac6c4e735d7245f4b0c4
Liana Kozhamuratova <lianakozamuratova@gmail.com>
2025-01-31 23:56:07 +0500

    Python lists (2 week)

---
+++ lists/lists.py
@@ -0,0 +1,22 @@
+mylist = ["apple", "banana", "cherry"]
+
+'''
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+'''
+
+'''
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+print(len(thislist))
+'''
+
+'''list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+'''
+
+'''
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+'''
