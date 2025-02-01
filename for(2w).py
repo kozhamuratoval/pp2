744edc7d6d3c77005fd1e60602ce0484543a936a
Liana Kozhamuratova <lianakozamuratova@gmail.com>
2025-02-01 11:40:54 +0500

    for loop

---
+++ for(2w).py
@@ -0,0 +1,25 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+for x in "banana":
+  print(x)
+
+'''fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break'''
+
+'''fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)'''
+
+'''adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)'''
