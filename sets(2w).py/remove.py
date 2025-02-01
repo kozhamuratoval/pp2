6e6a22db63453a7bdefcc87e622dd05b3359dc63
Liana Kozhamuratova <lianakozamuratova@gmail.com>
2025-02-01 11:05:49 +0500

    remove set items

---
+++ sets(2w).py/remove.py
@@ -0,0 +1,14 @@
+thisset = {"apple", "banana", "cherry"}
+thisset.remove("banana")
+print(thisset)
+
+thisset.discard("kiwi")
+print(thisset)
+
+thisset.clear()
+print(thisset)
+
+'''thisset = {"apple", "banana", "cherry"}
+del thisset
+print(thisset)
+'''
