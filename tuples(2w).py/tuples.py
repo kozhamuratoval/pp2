c57a646831740773cbad29278ac9f73134e5fc08
Liana Kozhamuratova <lianakozamuratova@gmail.com>
2025-02-01 10:52:20 +0500

    tuples

---
+++ tuples(2w).py/tuples.py
@@ -0,0 +1,19 @@
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+print(len(thistuple))
+
+'''
+thistuple = ("apple",)
+print(type(thistuple))
+
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
+'''
+
+'''
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+tuple4 = ("abc", 34, True, 40, "male")
+'''
