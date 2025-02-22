97ad2b72b7fc41d0f126eee60f3739b0035728c4
Liana Kozhamuratova <lianakozamuratova@gmail.com>
2025-02-22 19:35:31 +0500

    math

---
+++ week4/math.py
@@ -0,0 +1,21 @@
+import math
+"""z = int(input())
+print(math.radians(z))"""
+
+'''height = int(input())
+print("base, first value: ")
+base1 = int(input())
+print("base, second value: ")
+base2 = int(input())
+area = ((base1 + base2)*height)/2
+print(area)'''
+
+'''r = int(input())
+length = int(input())
+area = (r * (length**2))/(4 * math.tan(math.pi)/r)
+print(area)'''
+
+'''a = float(input())
+h = float(input())
+print(a*h)'''
+
